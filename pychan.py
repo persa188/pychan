cc614194699a0f6dc11f879d0b21656eca7cd3ae
persa188 <dev.persaud@mail.utoronto.ca>
2015-12-25 16:21:28 -0500

    added cmd args

--- pychan.py
+++ pychan.py
@@ -1,9 +1,10 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 import os
+import sys
 
-def get_images():
-	url = "http://boards.4chan.org/wg/"
+def get_images(url):
+
 	text = urllib2.urlopen(url).read();
 	soup = BeautifulSoup(text);
 
@@ -19,4 +20,13 @@ def get_images():
 			localfile.write(img.read())
 		incr += 1
 
-get_images()
+#this is for handling command line args
+if not sys.argv[1:]:
+	_url_ = 'http://boards.4chan.org/w/'
+elif sys.argv[1] == '-g' or sys.argv[1] == '--general':
+	_url_ = 'http://boards.4chan.org/wg/'
+else:
+	print >> sys.stderr, 'usage: too many args'
+	exit(127)
+
+get_images(_url_)
